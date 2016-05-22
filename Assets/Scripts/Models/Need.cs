855e46f6cdfdb78ab313995f5f47bbf97de73122
mortmann <st111523@stud.uni-stuttgart.de>
2016-05-22 21:22:41 +0200

    Starting to create Citizen and their Needs.

---
+++ Assets/Scripts/Models/Need.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Need {
+	int id;
+	string name;
+	Item item;
+	Structure structure;
+	float[] uses;
+	int level;
+	bool isInrange;
+	public Need(int id, string name,int level, Item item,Structure structure, float[] uses){
+		this.id = id;
+		this.name = name;
+		this.level = level;
+		this.item = item;
+		this.uses = uses;
+		this.structure = structure;
+	}
+	public float TryToConsumThis(City city,int level,int people){
+		if(this.level > level){
+			//whoever called this doesnt have the permissions to consum
+			return;
+		}
+		if(item == null){
+			//this does not require any item -> it needs a structure
+			return;
+		}
+		float neededCounsumAmount = uses [level] * ((float)people);
+		float gottenAmount = city.TryToGetAmount (item);
+
+		return gottenAmount / neededCounsumAmount;
+	}
+}
---
+++ Assets/Scripts/Models/Structures/HomeBuilding.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+public class HomeBuilding : Structure{
+
+	public int people;
+	public int maxLivingSpaces;
+	public float increaseSpeed;
+	public float decreaseSpeed;
+	public bool canUpgrade;
+	public int buildingLevel;
+
+	public HomeBuilding(){
+		
+		canUpgrade = false;
+		tileWidth = 2;
+		tileHeight = 2;
+		BuildTyp = BuildTypes.Drag;
+		myBuildingTyp =	BuildingTyp.Blocking;
+		people = 1;
+		maxLivingSpaces = 8;
+		buildingLevel = 0;
+		name = "Home";
+	}
+	protected HomeBuilding(HomeBuilding b){
+		this.people = b.people;
+		this.tileWidth = b.tileWidth;
+		this.tileHeight = b.tileHeight;
+		this.rotated = b.rotated;
+		this.maintenancecost = b.maintenancecost;
+		this.hasHitbox = b.hasHitbox;
+		this.name = b.name;
+		this.BuildTyp = b.BuildTyp;
+		this.buildcost = b.buildcost;
+		this.buildingLevel = 1;
+		this.maintenancecost = 0;
+		this.maxLivingSpaces = b.maxLivingSpaces;
+	}
+	public override Structure Clone (){
+		return new HomeBuilding (this);
+	}
+
+	public override void OnBuild(){
+		
+	}
+
+	public override void update (float deltaTime) {
+	
+	}
+
+	public override void WriteXml (XmlWriter writer)	{
+		writer.WriteAttributeString("Name", name ); //change this to id
+		writer.WriteAttributeString("BuildingTile_X", myBuildingTiles[0].X.ToString () );
+		writer.WriteAttributeString("BuildingTile_Y", myBuildingTiles[0].Y.ToString () );
+		writer.WriteElementString("Rotated", rotated.ToString());
+
+
+
+	}
+	public override void ReadXml (XmlReader reader) {
+		
+	}
+}
